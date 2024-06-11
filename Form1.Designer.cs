797ddf2073d06a520f24d3fe4626d62864657e91
jmtoledo175 <juli.toledo175@gmail.com>
2024-06-11 15:52:27 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace pryDosVectores
+{
+    partial class fmrDosVectores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbMes = new System.Windows.Forms.ComboBox();
+            this.cmbImporte = new System.Windows.Forms.ComboBox();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnFor = new System.Windows.Forms.Button();
+            this.btnWhile = new System.Windows.Forms.Button();
+            this.dgvListado = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCargar);
+            this.groupBox1.Controls.Add(this.cmbImporte);
+            this.groupBox1.Controls.Add(this.cmbMes);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(32, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(342, 151);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Carga de datos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mes:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Importe:";
+            // 
+            // cmbMes
+            // 
+            this.cmbMes.FormattingEnabled = true;
+            this.cmbMes.Location = new System.Drawing.Point(134, 19);
+            this.cmbMes.Name = "cmbMes";
+            this.cmbMes.Size = new System.Drawing.Size(153, 21);
+            this.cmbMes.TabIndex = 2;
+            // 
+            // cmbImporte
+            // 
+            this.cmbImporte.FormattingEnabled = true;
+            this.cmbImporte.Location = new System.Drawing.Point(134, 62);
+            this.cmbImporte.Name = "cmbImporte";
+            this.cmbImporte.Size = new System.Drawing.Size(153, 21);
+            this.cmbImporte.TabIndex = 3;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(134, 100);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(153, 33);
+            this.btnCargar.TabIndex = 4;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvListado);
+            this.groupBox2.Controls.Add(this.btnWhile);
+            this.groupBox2.Controls.Add(this.btnFor);
+            this.groupBox2.Location = new System.Drawing.Point(32, 209);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(348, 215);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Listado de datos";
+            // 
+            // btnFor
+            // 
+            this.btnFor.Location = new System.Drawing.Point(38, 174);
+            this.btnFor.Name = "btnFor";
+            this.btnFor.Size = new System.Drawing.Size(117, 25);
+            this.btnFor.TabIndex = 0;
+            this.btnFor.Text = "Listar con For";
+            this.btnFor.UseVisualStyleBackColor = true;
+            this.btnFor.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnWhile
+            // 
+            this.btnWhile.Location = new System.Drawing.Point(199, 174);
+            this.btnWhile.Name = "btnWhile";
+            this.btnWhile.Size = new System.Drawing.Size(117, 25);
+            this.btnWhile.TabIndex = 1;
+            this.btnWhile.Text = "Listar con While";
+            this.btnWhile.UseVisualStyleBackColor = true;
+            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
+            // 
+            // dgvListado
+            // 
+            this.dgvListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListado.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dgvListado.Location = new System.Drawing.Point(58, 19);
+            this.dgvListado.Name = "dgvListado";
+            this.dgvListado.Size = new System.Drawing.Size(241, 150);
+            this.dgvListado.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Mes";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Importe";
+            this.Column2.Name = "Column2";
+            // 
+            // fmrDosVectores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "fmrDosVectores";
+            this.Text = "Procesando dos vectores";
+            this.Load += new System.EventHandler(this.fmrDosVectores_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.ComboBox cmbImporte;
+        private System.Windows.Forms.ComboBox cmbMes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnFor;
+        private System.Windows.Forms.Button btnWhile;
+        private System.Windows.Forms.DataGridView dgvListado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryDosVectores
+{
+    public partial class fmrDosVectores : Form
+    {
+        public fmrDosVectores()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dgvListado.Rows.Clear();
+            for (int i = 0; i < Meses.Length; i++)
+            {
+                dgvListado.Rows.Add(Meses[i],Importe[i]);
+            }
+        }
+
+        string[] Meses = new string[12] {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+        decimal[] Importe = new decimal[12];
+        Int32 indice = 0;
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            Int32 i = cmbMes.SelectedIndex; 
+            decimal importe = Convert.ToDecimal(cmbImporte.Text);
+            Importe[i] = Importe[i]+importe;
+            MessageBox.Show("Dato cargado");
+        }
+
+        private void fmrDosVectores_Load(object sender, EventArgs e)
+        {   
+            cmbMes.Items.Clear();
+            for (int i = 0; i < Meses.Length; i++) {
+                cmbMes.Items.Add(Meses[i]);
+            }
+            cmbMes.SelectedIndex = 0;      
+        }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+            cmbMes.Items.Clear();
+            int i = 0;
+            while (i < Meses.Length)
+            {
+                dgvListado.Rows.Add(Meses[i], Importe[i]);
+                i++;
+            }
+        }
+    }
+}
